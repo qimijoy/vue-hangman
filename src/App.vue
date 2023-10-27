98e695f04648dcdea18e44ce2031dccc3c18f064
qimijoy <qimijoy@yandex.ru>
2023-10-27 12:30:06 +0300

    hangman picture

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
 	<div class="hangman">
 		<HHeader />
+
+		<main class="hangman__content">
+			<HHangmanPicture mistakes="0" />
+		</main>
 	</div>
 </template>
 
 <script setup>
 import HHeader from '@/components/HHeader.vue';
+import HHangmanPicture from '@/components/HHangmanPicture.vue';
 </script>
 
 <style lang="less" scoped>
@@ -17,5 +22,12 @@ import HHeader from '@/components/HHeader.vue';
 	max-width: 450px;
 	padding: 20px 30px;
 	margin: 0 auto;
+
+	&__content {
+		position: relative;
+		height: 350px;
+		padding: 20px 0;
+		margin: auto;
+	}
 }
 </style>
