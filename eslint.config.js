12f43be527958a13d07b7e1c61a1c0aa4a1eb176
qimijoy <qimijoy@yandex.ru>
2023-10-27 10:56:30 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,72 @@
+const globals = require('globals');
+
+const babelParser = require('@babel/eslint-parser');
+const eslintConfig = require('@qimijoy/eslint-config/configs/primary');
+
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+const vueParser = require('vue-eslint-parser');
+const vue = require('eslint-plugin-vue');
+const vueConfig = require('@qimijoy/eslint-config/configs/vueConfig');
+
+module.exports = [
+	{
+		linterOptions: {
+			noInlineConfig: true,
+			reportUnusedDisableDirectives: true,
+		},
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021,
+			},
+		},
+	},
+	{
+		files: ['**/*.js'],
+		ignores: ['**/dist/**'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parser: babelParser,
+			parserOptions: {
+				babelOptions: {
+					presets: ['@babel/preset-env'],
+				},
+			},
+		},
+		rules: {
+			...eslintConfig.rules,
+		},
+	},
+	// VUE
+	{
+		files: ['**/*.vue'],
+		ignores: ['**/dist/**'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			parser: vueParser, // <template>
+			parserOptions: {
+				parser: babelParser, // <script>
+			},
+		},
+		plugins: { vue },
+		rules: {
+			...vueConfig.rules,
+		},
+	},
+	// Prettier options (must be last!)
+	{
+		files: ['**/*.js'],
+		ignores: ['**/dist/**'],
+		plugins: { prettier },
+		rules: {
+			...prettierConfig.rules,
+			'prettier/prettier': 'error',
+			'arrow-body-style': 'off',
+			'prefer-arrow-callback': 'off',
+		},
+	},
+];
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+	plugins: [vue()],
+	base: process.env.NODE_ENV === 'production' ? '/skillometer/' : '/',
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		outDir: './dist/',
+		cssCodeSplit: false,
+	},
+});
